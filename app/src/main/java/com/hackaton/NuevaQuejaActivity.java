e6ea614aa18dba40b9dbc20382008591ab5df0f4
Leo <lrmaldo@gmail.com>
2016-12-21 17:42:27 -0600

    Agregacion modificacion

--- app/src/main/java/com/hackaton/NuevaQuejaActivity.java
+++ app/src/main/java/com/hackaton/NuevaQuejaActivity.java
@@ -1,40 +1,62 @@
 package com.hackaton;
 
-import android.Manifest;
-import android.app.Activity;
-import android.content.ComponentName;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
-import android.graphics.Bitmap;
-import android.net.Uri;
-import android.os.Bundle;
-import android.os.Parcelable;
-import android.provider.MediaStore;
-import android.support.annotation.NonNull;
-import android.support.v4.app.FragmentManager;
-import android.text.TextUtils;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
+/*
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
 
-import pub.devrel.easypermissions.AfterPermissionGranted;
-import pub.devrel.easypermissions.EasyPermissions;
+
+        import android.Manifest;
+        import android.app.Activity;
+        import android.content.ComponentName;
+        import android.content.Intent;
+        import android.content.pm.PackageManager;
+        import android.content.pm.ResolveInfo;
+        import android.graphics.Bitmap;
+        import android.net.Uri;
+        import android.os.Build;
+        import android.os.Bundle;
+        import android.os.Environment;
+        import android.os.Parcelable;
+        import android.provider.MediaStore;
+        import android.support.annotation.NonNull;
+        import android.support.v4.app.FragmentManager;
+        import android.text.TextUtils;
+        import android.util.Log;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.ImageView;
+        import android.widget.TextView;
+        import android.widget.Toast;
+
+        import java.io.File;
+        import java.util.ArrayList;
+        import java.util.List;
+        import java.util.UUID;
+
+        import pub.devrel.easypermissions.AfterPermissionGranted;
+        import pub.devrel.easypermissions.EasyPermissions;
+
+
 public class NuevaQuejaActivity extends BaseActivity implements
         EasyPermissions.PermissionCallbacks,
         NewPostUploadTaskFragment.TaskCallbacks {
     public static final String TAG = "NewPostActivity";
     public static final String TAG_TASK_FRAGMENT = "newPostUploadTaskFragment";
-    private static final int THUMBNAIL_MAX_DIMENSION = 640;
+    private static final int THUMBNAIL_MAX_DIMENSION = 720;
     private static final int FULL_SIZE_MAX_DIMENSION = 1280;
     private Button mSubmitButton;
 
@@ -47,6 +69,11 @@ public class NuevaQuejaActivity extends BaseActivity implements
 
     private static final int TC_PICK_IMAGE = 101;
     private static final int RC_CAMERA_PERMISSIONS = 102;
+
+
+    private static final int RC_TAKE_PICTURE = 101;
+    private static final int RC_STORAGE_PERMS = 102;
+
 
     private static final String[] cameraPerms = new String[]{
             Manifest.permission.READ_EXTERNAL_STORAGE
@@ -92,11 +119,7 @@ public class NuevaQuejaActivity extends BaseActivity implements
         mSubmitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View v) {
-                if (mResizedBitmap == null) {
-                    Toast.makeText(NuevaQuejaActivity.this, "Select an image first.",
-                            Toast.LENGTH_SHORT).show();
-                    return;
-                }
+
                 String postText = descriptionText.getText().toString();
                 if (TextUtils.isEmpty(postText)) {
                     descriptionText.setError(getString(R.string.error_required_field));
@@ -107,14 +130,13 @@ public class NuevaQuejaActivity extends BaseActivity implements
 
                 Long timestamp = System.currentTimeMillis();
 
-                String bitmapPath = "/" + FirebaseUtil.getCurrentUserId() + "/full/" + timestamp.toString() + "/";
-                String thumbnailPath = "/" + FirebaseUtil.getCurrentUserId() + "/thumb/" + timestamp.toString() + "/";
+                String bitmapPath = "/" + FirebaseUtil.getCurrentUserId() + "/full/" + mFileUri + "/";
+                String thumbnailPath = "/" + FirebaseUtil.getCurrentUserId() + "/thumb/" + mFileUri + "/";
                 mTaskFragment.uploadPost(mResizedBitmap, bitmapPath, mThumbnail, thumbnailPath, mFileUri.getLastPathSegment(),
                         postText);
             }
         });
     }
-
 
     @Override
     public void onPostUploaded(final String error) {
@@ -124,10 +146,10 @@ public class NuevaQuejaActivity extends BaseActivity implements
                 mSubmitButton.setEnabled(true);
                 dismissProgressDialog();
                 if (error == null) {
-                    Toast.makeText(getApplicationContext(), "Queja enviado", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(NuevaQuejaActivity.this, "Post created!", Toast.LENGTH_SHORT).show();
                     finish();
                 } else {
-                    Toast.makeText(getApplicationContext(), error, Toast.LENGTH_SHORT).show();
+                    Toast.makeText(NuevaQuejaActivity.this, error, Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -136,62 +158,45 @@ public class NuevaQuejaActivity extends BaseActivity implements
     @AfterPermissionGranted(RC_CAMERA_PERMISSIONS)
     private void showImagePicker() {
         // Check for camera permissions
-        if (!EasyPermissions.hasPermissions(this, cameraPerms)) {
-            EasyPermissions.requestPermissions(this,
-                    "This sample will upload a picture from your Camera",
-                    RC_CAMERA_PERMISSIONS, cameraPerms);
+        Log.d(TAG, "launchCamera");
+
+        // Check that we have permission to read images from external storage.
+        String perm = Manifest.permission.READ_EXTERNAL_STORAGE;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && !EasyPermissions.hasPermissions(this, perm)) {
+            EasyPermissions.requestPermissions(this, "This sample reads images from your camera to demonstrate uploading.",
+                    RC_STORAGE_PERMS, perm);
             return;
         }
 
+        // Create intent
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
         // Choose file storage location
-        File file = new File(getExternalCacheDir(), UUID.randomUUID().toString());
+        File file = new File(Environment.getExternalStorageDirectory(), UUID.randomUUID().toString() + ".jpg");
         mFileUri = Uri.fromFile(file);
+        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, mFileUri);
 
-        // Camera
-        final List<Intent> cameraIntents = new ArrayList<Intent>();
-        final Intent captureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        final PackageManager packageManager = getPackageManager();
-        final List<ResolveInfo> listCam = packageManager.queryIntentActivities(captureIntent, 0);
-        for (ResolveInfo res : listCam){
-            final String packageName = res.activityInfo.packageName;
-            final Intent intent = new Intent(captureIntent);
-            intent.setComponent(new ComponentName(packageName, res.activityInfo.name));
-            intent.setPackage(packageName);
-            intent.putExtra(MediaStore.EXTRA_OUTPUT, mFileUri);
-            cameraIntents.add(intent);
-        }
-
-        // Image Picker
-        Intent pickerIntent = new Intent(Intent.ACTION_PICK,
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-
-        Intent chooserIntent = Intent.createChooser(pickerIntent,
-                getString(R.string.picture_chooser_title));
-        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, cameraIntents.toArray(new
-                Parcelable[cameraIntents.size()]));
-        startActivityForResult(chooserIntent, TC_PICK_IMAGE);
+        // Launch intent
+        startActivityForResult(takePictureIntent, RC_TAKE_PICTURE);
     }
 
     @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == TC_PICK_IMAGE) {
-            if (resultCode == Activity.RESULT_OK) {
-                final boolean isCamera;
-                if (data.getData() == null) {
-                    isCamera = true;
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.d(TAG, "onActivityResult:" + requestCode + ":" + resultCode + ":" + data);
+        if (requestCode == RC_TAKE_PICTURE) {
+            if (resultCode == RESULT_OK) {
+                if (mFileUri != null) {
+                    Log.d(TAG, "onActivityResult: "+ mFileUri );
                 } else {
-                    isCamera = MediaStore.ACTION_IMAGE_CAPTURE.equals(data.getAction());
+                    Log.w(TAG, "File URI is null");
                 }
-                if (!isCamera) {
-                    mFileUri = data.getData();
-                }
-                Log.d(TAG, "Received file uri: " + mFileUri.getPath());
-
                 mTaskFragment.resizeBitmap(mFileUri, THUMBNAIL_MAX_DIMENSION);
                 mTaskFragment.resizeBitmap(mFileUri, FULL_SIZE_MAX_DIMENSION);
+            } else {
+                Toast.makeText(this, "Taking picture failed.", Toast.LENGTH_SHORT).show();
             }
+
         }
     }
 
@@ -241,6 +246,5 @@ public class NuevaQuejaActivity extends BaseActivity implements
     @Override
     public void onPermissionsDenied(int requestCode, List<String> perms) {
     }
-
-
 }
+
